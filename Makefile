1f6343bba485b7f6c50b7d9647e9caa03528b448
zhs <haishan.zhou@magewell.com>
2017-05-02 11:29:31 +0800

    Makefile: modify 'new', remove 'del' and 'edit'

--- Makefile
+++ Makefile
@@ -28,11 +28,5 @@ pull: ## run git pull to update.
 new: ## execute `hugo new` to add a new post
 	@`pwd`/scripts/post.sh ${THEME} new 
 
-edit: ## execute `vim content/post` to edit a post
-	@`pwd`/scripts/post.sh ${THEME} edit 
-
-del: ## delete a post
-	@`pwd`/scripts/post.sh ${THEME} del
-
 help:
 	@grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- scripts/post.sh
+++ scripts/post.sh
@@ -8,15 +8,10 @@ CONFIG=data/$THEME.config.toml
 echo "Theme is $THEME"
 
 if [[ $CMD == "new" ]]; then
+	echo -n -e "input category directory name\n>> "
+	read dirname
 	echo -n -e "input post name\n>> "
-	read name
-	hugo --config $CONFIG new post/$name.md
-	exit $?
-fi
-
-if [[ $CMD == "del" ]]; then
-	echo -n -e "input post name\n>> "
-	read name
-	rm -v content/post/$name.md
+	read postname
+	hugo --config $CONFIG new $dirname/$postname.md
 	exit $?
 fi
