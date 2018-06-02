adc384c3aa77fc2cb262f5937f7f24cbd3ac0fa4
choueric <zhssmail@gmail.com>
2018-06-02 16:06:42 +0800

    script: fix hugo path

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -3,14 +3,14 @@
 BASEURL=http://ericnode.info
 BIND=ericnode.info
 #BIND=127.0.0.1
-HUGO=/home/zhs/bin/hugo 
-SITEDIR=/home/zhs/ericnode
+HUGO=$HOME/bin/hugo 
+SITEDIR=$HOME/ericnode
 CONFIG=$SITEDIR/data/mainroad.config.toml
 PORT=80
 FLAGS="--disableLiveReload=true"
 
 if [[ $1 == "test" ]]; then
-	hugo server -ws $SITEDIR --config $CONFIG \
+	$HUGO server -ws $SITEDIR --config $CONFIG \
 		--buildDrafts -b http://localhost
 fi
 
